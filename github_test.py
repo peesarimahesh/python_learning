d7d359aa81135913d02d51e2ba5ab73868226238
peesarimahesh <peesari.mahesh@gmail.com>
2019-11-13 06:26:20 -0500

    Testing

--- github_test.py
+++ github_test.py
@@ -12,3 +12,29 @@ print(x)
 import math
 x = 100
 print(math.sqrt(x))
+
+p = "I Love 'PYTHON' "
+print(p)
+
+p1 = "I Love \"Python \" "
+print(p1)
+
+print("c:\some\name")
+# to fix from going to new line "USE R"
+print(r"c:\some\name")
+
+# Use triple quotes to preserve pre-formatted string values
+s = """ Python
+is
+
+a nice language"""
+print(s)
+
+#to conatenate strings use "+" operator and to repeat use "*"
+s1 = "Hello "
+s2 = "World"
+s = s1+s2
+print(s)
+
+r = " Hello"
+print(r * 100)
